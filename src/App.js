5faff1bf3d926a3a86c0d4c000c2de357891965f
vchiuu <vivian.chiu@live.com>
2021-01-14 00:17:01 -0500

    set up + assets

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import FilmProjector from './assets/film-projector.svg';
+import ShoppiesTrophy from './assets/shoppies-trophy.svg';
+import Title from './assets/title.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="form">
+        <div className="call-to-action"> Nominate your Favourite Flicks for the </div>
+        <img className="title" src={Title} alt="the Shoppies" />
+      </div>
+      <img className="trophy" src={ShoppiesTrophy} alt="Shoppies Trophy" />
+      <img className="film-projector" src={FilmProjector} alt="projector" />
     </div>
   );
 }
